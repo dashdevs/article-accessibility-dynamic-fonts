2e44afe695b6da28f7a6723a435640c758b69e82
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2022-08-24 15:31:27 +0300

    test: Dynamic Type tests

--- AccessibilityArticleSnapshotTests/SwiftUISnapshotTests.swift
+++ AccessibilityArticleSnapshotTests/SwiftUISnapshotTests.swift
@@ -18,4 +18,23 @@ class SwiftUISnapshotTests: XCTestCase {
         let navigationController = UINavigationController(rootViewController: hostingController)
         assertSnapshot(matching: navigationController, as: .accessibilityImage)
     }
+
+    func test_intro_dynamicType() {
+        let introView = IntroView()
+        let hostingController = UIHostingController(rootView: introView)
+        hostingController.view.frame.size = CGSize(width: 375, height: 667)
+        assertSnapshot(matching: hostingController, as: .image(
+            traits: UITraitCollection(preferredContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+        ))
+
+    }
+
+    func test_details_dynamicType() {
+        let introView = DetailsView()
+        let hostingController = UIHostingController(rootView: introView)
+        let navigationController = UINavigationController(rootViewController: hostingController)
+        assertSnapshot(matching: navigationController, as: .image(
+            traits: UITraitCollection(preferredContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+        ))
+    }
 }
--- AccessibilityArticleSnapshotTests/UIKitSnapshotTests.swift
+++ AccessibilityArticleSnapshotTests/UIKitSnapshotTests.swift
@@ -14,4 +14,19 @@ class UIKitSnapshotTests: XCTestCase {
         let navigationController = UINavigationController(rootViewController: detailsViewController)
         assertSnapshot(matching: navigationController, as: .accessibilityImage)
     }
+
+    func test_intro_dynamicType() {
+        let introViewController = IntroViewController(nibName: "IntroViewController", bundle: .module)
+        assertSnapshot(matching: introViewController, as: .image(
+            traits: UITraitCollection(preferredContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+        ))
+    }
+
+    func test_details_dynamicType() {
+        let detailsViewController = DetailsViewController(nibName: "DetailsViewController", bundle: .module)
+        let navigationController = UINavigationController(rootViewController: detailsViewController)
+        assertSnapshot(matching: navigationController, as: .image(
+            traits: UITraitCollection(preferredContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+        ))
+    }
 }
