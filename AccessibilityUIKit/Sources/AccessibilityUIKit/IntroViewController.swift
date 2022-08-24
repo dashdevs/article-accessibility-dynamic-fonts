b6a5116a52c6434a2ffa64885fbcbfe755b0f722
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2022-08-24 11:50:18 +0300

    feat: Dynamic Type custom font

--- AccessibilityUIKit/Sources/AccessibilityUIKit/IntroViewController.swift
+++ AccessibilityUIKit/Sources/AccessibilityUIKit/IntroViewController.swift
@@ -10,6 +10,22 @@ class IntroViewController: UIViewController {
         imageView.isAccessibilityElement = true
         imageView.accessibilityLabel = "Accessibility image"
         imageView.accessibilityHint = "Accessibility icon on the Intro screen"
+        if let font = UIFont(name: "Roboto-Regular", size: 28) {
+            label.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: font, maximumPointSize: 38)
+            label.adjustsFontForContentSizeCategory = true
+        } else {
+            assertionFailure("Failed to load Roboto-Regular font!")
+        }
+        if let font = UIFont(name: "Roboto-Regular", size: 17) {
+            let scaledFont = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+            let title = NSAttributedString(
+                string: "Open List Screen New Title",
+                attributes: [.font: scaledFont]
+            )
+            button.setAttributedTitle(title, for: .normal)
+        } else {
+            assertionFailure("Failed to load Roboto-Regular font!")
+        }
     }
 
     @IBAction private func openListScreen() {
